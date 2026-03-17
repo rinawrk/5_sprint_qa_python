6130ae740695560551e6f874238f4c1dbbf0baa8
Irina Mikheeva <mikheeva.wrk@gmail.com>
2026-03-18 01:44:08 +0800

    locators.py - подготовка локаторов страницы входа

--- locators.py
+++ locators.py
@@ -7,4 +7,11 @@ REGISTER_BUTTON = (By.XPATH, "//button[text()='Зарегистрировать�
 LOGIN_LINK = (By.XPATH, "//a[text()='Войти']")
 INCORRECT_PASSWORD_ERROR = (By.XPATH, "//p[text()='Некорректный пароль']")
 
+# Локаторы для страницы входа
+
+EMAIL_INPUT = (By.XPATH, "//label[text()='Email']/../input")
+PASSWORD_INPUT = (By.CSS_SELECTOR, "input[type='password']")
+LOGIN_BUTTON = (By.XPATH, "//button[text()='Войти']")
+REGISTER_LINK = (By.XPATH, "//a[text()='Зарегистрироваться']")
+FORGOT_PASSWORD_LINK = (By.XPATH, "//a[text()='Восстановить пароль']")
 
