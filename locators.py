9ef757e322a19d29ad8f673e2231bcf98ef94b72
Irina Mikheeva <mikheeva.wrk@gmail.com>
2026-03-18 02:20:57 +0800

    locators.py - локаторы подготовлены

--- locators.py
+++ locators.py
@@ -1,38 +1,95 @@
-# Локаторы для страницы регистрации
+from selenium.webdriver.common.by import By
 
-NAME_INPUT = (By.XPATH, "//label[text()='Имя']/../input")
-EMAIL_INPUT = (By.XPATH, "//label[text()='Email']/../input")
-PASSWORD_INPUT = (By.CSS_SELECTOR, "input[type='password']")
-REGISTER_BUTTON = (By.XPATH, "//button[text()='Зарегистрироваться']")
-LOGIN_LINK = (By.XPATH, "//a[text()='Войти']")
-INCORRECT_PASSWORD_ERROR = (By.XPATH, "//p[text()='Некорректный пароль']")
 
-# Локаторы для страницы входа
+class RegisterPageLocators:
+    # Поле ввода имени на странице регистрации
+    NAME_INPUT = (By.XPATH, "//label[text()='Имя']/../input")
 
-EMAIL_INPUT = (By.XPATH, "//label[text()='Email']/../input")
-PASSWORD_INPUT = (By.CSS_SELECTOR, "input[type='password']")
-LOGIN_BUTTON = (By.XPATH, "//button[text()='Войти']")
-REGISTER_LINK = (By.XPATH, "//a[text()='Зарегистрироваться']")
-FORGOT_PASSWORD_LINK = (By.XPATH, "//a[text()='Восстановить пароль']")
+    # Поле ввода email на странице регистрации
+    EMAIL_INPUT = (By.XPATH, "//label[text()='Email']/../input")
 
-# Локаторы для страницы восстановления пароля
+    # Поле ввода пароля на странице регистрации
+    PASSWORD_INPUT = (By.XPATH, "//label[text()='Пароль']/../input")
 
-LOGIN_LINK = (By.XPATH, "//a[text()='Войти']")
+    # Кнопка "Зарегистрироваться" на странице регистрации
+    REGISTER_BUTTON = (By.XPATH, "//button[text()='Зарегистрироваться']")
 
-# Локаторы для страницы профиля
+    # Ссылка "Войти" на странице регистрации
+    LOGIN_LINK = (By.XPATH, "//a[text()='Войти']")
 
-LOGOUT_BUTTON = (By.XPATH, "//button[text()='Выход']")
+    # Сообщение об ошибке при некорректном пароле
+    INCORRECT_PASSWORD_ERROR = (By.XPATH, "//p[text()='Некорректный пароль']")
 
-# Локаторы для конструктора
 
-BUNS_TAB = (By.XPATH, "//span[text()='Булки']/..")
-SAUCES_TAB = (By.XPATH, "//span[text()='Соусы']/..")
-FILLINGS_TAB = (By.XPATH, "//span[text()='Начинки']/..")
+class LoginPageLocators:
+    # Поле ввода email на странице входа
+    EMAIL_INPUT = (By.XPATH, "//label[text()='Email']/../input")
 
-# Локаторы для главной страницы
+    # Поле ввода пароля на странице входа
+    PASSWORD_INPUT = (By.XPATH, "//label[text()='Пароль']/../input")
 
-LOGIN_BUTTON = (By.XPATH, "//button[text()='Войти в аккаунт']")
-PERSONAL_ACCOUNT_BUTTON = (By.XPATH, "//p[text()='Личный Кабинет']")
-CONSTRUCTOR_BUTTON = (By.XPATH, "//p[text()='Конструктор']")
-LOGO = (By.CSS_SELECTOR, "div.AppHeader_header__logo__2D0X2 a")
-PLACE_ORDER_BUTTON = (By.XPATH, "//button[text()='Оформить заказ']")
+    # Кнопка "Войти" на странице входа
+    LOGIN_BUTTON = (By.XPATH, "//button[text()='Войти']")
+
+    # Ссылка "Зарегистрироваться" на странице входа
+    REGISTER_LINK = (By.XPATH, "//a[text()='Зарегистрироваться']")
+
+    # Ссылка "Восстановить пароль" на странице входа
+    FORGOT_PASSWORD_LINK = (By.XPATH, "//a[text()='Восстановить пароль']")
+
+
+class ForgotPasswordPageLocators:
+    # Ссылка "Войти" на странице восстановления пароля
+    LOGIN_LINK = (By.XPATH, "//a[text()='Войти']")
+
+
+class ProfilePageLocators:
+    # Кнопка "Выход" в личном кабинете
+    LOGOUT_BUTTON = (By.XPATH, "//button[text()='Выход']")
+
+
+class MainPageLocators:
+    # Кнопка "Войти в аккаунт" на главной странице
+    LOGIN_BUTTON = (By.XPATH, "//button[text()='Войти в аккаунт']")
+
+    # Кнопка "Личный Кабинет" в шапке сайта
+    PERSONAL_ACCOUNT_BUTTON = (By.XPATH, "//p[text()='Личный Кабинет']")
+
+    # Кнопка "Конструктор" в шапке сайта
+    CONSTRUCTOR_BUTTON = (By.XPATH, "//p[text()='Конструктор']")
+
+    # Логотип Stellar Burgers в шапке сайта
+    LOGO = (By.CSS_SELECTOR, "div[class*='AppHeader_header__logo'] a")
+
+    # Кнопка "Оформить заказ" для авторизованного пользователя
+    PLACE_ORDER_BUTTON = (By.XPATH, "//button[text()='Оформить заказ']")
+
+
+class ConstructorPageLocators:
+    # Вкладка "Булки" в конструкторе
+    BUNS_TAB = (By.XPATH, "//span[text()='Булки']/..")
+
+    # Вкладка "Соусы" в конструкторе
+    SAUCES_TAB = (By.XPATH, "//span[text()='Соусы']/..")
+
+    # Вкладка "Начинки" в конструкторе
+    FILLINGS_TAB = (By.XPATH, "//span[text()='Начинки']/..")
+
+    # Активная вкладка "Булки" в конструкторе
+    ACTIVE_BUNS_TAB = (
+        By.XPATH,
+        "//div[contains(@class, 'tab_tab_type_current')]//span[text()='Булки']"
+    )
+
+    # Активная вкладка "Соусы" в конструкторе
+    ACTIVE_SAUCES_TAB = (
+        By.XPATH,
+        "//div[contains(@class, 'tab_tab_type_current')]//span[text()='Соусы']"
+    )
+
+    # Активная вкладка "Начинки" в конструкторе
+    ACTIVE_FILLINGS_TAB = (
+        By.XPATH,
+        "//div[contains(@class, 'tab_tab_type_current')]//span[text()='Начинки']"
+    )
+    
