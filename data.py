a83035b1a5e113255a25edbc72f0c5759258f0e6
Irina Mikheeva <mikheeva.wrk@gmail.com>
2026-03-18 00:16:04 +0800

    Добавлены константы в data.py

---
+++ data.py
@@ -0,0 +1,8 @@
+BASE_URL = "https://stellarburgers.education-services.ru/"
+LOGIN_URL = "https://stellarburgers.education-services.ru/login"
+REGISTER_URL = "https://stellarburgers.education-services.ru/register"
+PROFILE_URL = "https://stellarburgers.education-services.ru/account/profile"
+FORGOT_PASSWORD_URL = "https://stellarburgers.education-services.ru/forgot-password"
+
+EXISTING_USER_EMAIL = "irina_mikheeva_41_000@yandex.ru"
+EXISTING_USER_PASSWORD = "P@ssword"
