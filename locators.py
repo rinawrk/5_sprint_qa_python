6435ecba9ab8d979cb2001b7d4db799eed463cb4
Irina Mikheeva <mikheeva.wrk@gmail.com>
2026-03-18 02:04:44 +0800

    locators.py - подготовка локаторов

--- locators.py
+++ locators.py
@@ -18,3 +18,21 @@ FORGOT_PASSWORD_LINK = (By.XPATH, "//a[text()='Восстановить паро
 # Локаторы для страницы восстановления пароля
 
 LOGIN_LINK = (By.XPATH, "//a[text()='Войти']")
+
+# Локаторы для страницы профиля
+
+LOGOUT_BUTTON = (By.XPATH, "//button[text()='Выход']")
+
+# Локаторы для конструктора
+
+BUNS_TAB = (By.XPATH, "//span[text()='Булки']/..")
+SAUCES_TAB = (By.XPATH, "//span[text()='Соусы']/..")
+FILLINGS_TAB = (By.XPATH, "//span[text()='Начинки']/..")
+
+# Локаторы для главной страницы
+
+LOGIN_BUTTON = (By.XPATH, "//button[text()='Войти в аккаунт']")
+PERSONAL_ACCOUNT_BUTTON = (By.XPATH, "//p[text()='Личный Кабинет']")
+CONSTRUCTOR_BUTTON = (By.XPATH, "//p[text()='Конструктор']")
+LOGO = (By.CSS_SELECTOR, "div.AppHeader_header__logo__2D0X2 a")
+PLACE_ORDER_BUTTON = (By.XPATH, "//button[text()='Оформить заказ']")
