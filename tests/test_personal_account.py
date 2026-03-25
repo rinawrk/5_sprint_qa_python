47ec2f8bedf1a71c8b4cfcc80b09809fef6e2128
Irina Mikheeva <mikheeva.wrk@gmail.com>
2026-03-25 16:02:49 +0800

    test_personal_account.py - переход из личного кабинета в конструктор по логотипу Stellar Burgers

--- tests/test_personal_account.py
+++ tests/test_personal_account.py
@@ -40,7 +40,7 @@ class TestPersonalAccount:
     # Проверка перехода из личного кабинета в конструктор по кнопке "Конструктор"
 
     def test_go_to_constructor_by_constructor_button(self, driver):
-
+        
         # Открываем страницу входа
         driver.get(LOGIN_URL)
 
@@ -66,6 +66,44 @@ class TestPersonalAccount:
 
         # Нажимаем кнопку "Конструктор"
         driver.find_element(*MainPageLocators.CONSTRUCTOR_BUTTON).click()
+
+        # Ждём загрузки главной страницы
+        WebDriverWait(driver, 5).until(
+            EC.visibility_of_element_located(MainPageLocators.PLACE_ORDER_BUTTON)
+        )
+
+        # Проверяем, что открылась главная страница
+        assert driver.current_url == BASE_URL
+
+    # Проверка перехода из личного кабинета в конструктор по логотипу Stellar Burgers
+
+    def test_go_to_constructor_by_logo(self, driver):
+
+        # Открываем страницу входа
+        driver.get(LOGIN_URL)
+
+        # Вводим данные существующего пользователя
+        driver.find_element(*LoginPageLocators.EMAIL_INPUT).send_keys(EXISTING_USER_EMAIL)
+        driver.find_element(*LoginPageLocators.PASSWORD_INPUT).send_keys(EXISTING_USER_PASSWORD)
+
+        # Нажимаем кнопку "Войти"
+        driver.find_element(*LoginPageLocators.LOGIN_BUTTON).click()
+
+        # Ждём загрузки главной страницы авторизованного пользователя
+        WebDriverWait(driver, 5).until(
+            EC.visibility_of_element_located(MainPageLocators.PLACE_ORDER_BUTTON)
+        )
+
+        # Переходим в личный кабинет
+        driver.find_element(*MainPageLocators.PERSONAL_ACCOUNT_BUTTON).click()
+
+        # Ждём загрузки страницы профиля
+        WebDriverWait(driver, 5).until(
+            EC.visibility_of_element_located(ProfilePageLocators.LOGOUT_BUTTON)
+        )
+
+        # Нажимаем на логотип Stellar Burgers
+        driver.find_element(*MainPageLocators.LOGO).click()
 
         # Ждём загрузки главной страницы
         WebDriverWait(driver, 5).until(
