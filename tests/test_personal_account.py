f7714207b75080a5542250ef8811f2a06c89d762
Irina Mikheeva <mikheeva.wrk@gmail.com>
2026-03-25 16:05:05 +0800

    test_personal_account.py - выход из аккаунта по кнопке Выход

--- tests/test_personal_account.py
+++ tests/test_personal_account.py
@@ -78,7 +78,7 @@ class TestPersonalAccount:
     # Проверка перехода из личного кабинета в конструктор по логотипу Stellar Burgers
 
     def test_go_to_constructor_by_logo(self, driver):
-
+        
         # Открываем страницу входа
         driver.get(LOGIN_URL)
 
@@ -112,3 +112,42 @@ class TestPersonalAccount:
 
         # Проверяем, что открылась главная страница
         assert driver.current_url == BASE_URL
+
+    # Проверка выхода из аккаунта по кнопке "Выход"
+
+    def test_logout_from_personal_account(self, driver):
+
+        # Открываем страницу входа
+        driver.get(LOGIN_URL)
+
+        # Вводим данные существующего пользователя
+        driver.find_element(*LoginPageLocators.EMAIL_INPUT).send_keys(EXISTING_USER_EMAIL)
+        driver.find_element(*LoginPageLocators.PASSWORD_INPUT).send_keys(EXISTING_USER_PASSWORD)
+
+        # Нажимаем кнопку "Войти"
+        driver.find_element(*LoginPageLocators.LOGIN_BUTTON).click()
+
+        # Ждём загрузки главной страницы авторизованного пользователя
+        WebDriverWait(driver, 5).until(
+            EC.visibility_of_element_located(MainPageLocators.PLACE_ORDER_BUTTON)
+        )
+
+        # Переходим в личный кабинет
+        driver.find_element(*MainPageLocators.PERSONAL_ACCOUNT_BUTTON).click()
+
+        # Ждём загрузки страницы профиля
+        WebDriverWait(driver, 5).until(
+            EC.visibility_of_element_located(ProfilePageLocators.LOGOUT_BUTTON)
+        )
+
+        # Нажимаем кнопку "Выход"
+        driver.find_element(*ProfilePageLocators.LOGOUT_BUTTON).click()
+
+        # Ждём загрузки страницы входа
+        WebDriverWait(driver, 5).until(
+            EC.visibility_of_element_located(LoginPageLocators.LOGIN_BUTTON)
+        )
+
+        # Проверяем, что открылась страница входа
+        assert driver.current_url == LOGIN_URL
+        
