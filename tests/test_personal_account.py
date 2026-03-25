757157089d1cdeac74952a60068e604e39126c5a
Irina Mikheeva <mikheeva.wrk@gmail.com>
2026-03-25 16:00:17 +0800

    test_personal_account.py - переход из личного кабинета в конструктор по кнопке Конструктор

--- tests/test_personal_account.py
+++ tests/test_personal_account.py
@@ -10,7 +10,7 @@ class TestPersonalAccount:
     # Проверка перехода в личный кабинет по кнопке "Личный Кабинет"
 
     def test_go_to_personal_account(self, driver):
-        
+
         # Открываем страницу входа
         driver.get(LOGIN_URL)
 
@@ -36,3 +36,41 @@ class TestPersonalAccount:
 
         # Проверяем, что открылась страница личного кабинета
         assert driver.current_url == PROFILE_URL
+
+    # Проверка перехода из личного кабинета в конструктор по кнопке "Конструктор"
+
+    def test_go_to_constructor_by_constructor_button(self, driver):
+
+        # Открываем страницу входа
+        driver.get(LOGIN_URL)
+
+        # Вводим данные существующего пользователя
+        driver.find_element(*LoginPageLocators.EMAIL_INPUT).send_keys(EXISTING_USER_EMAIL)
+        driver.find_element(*LoginPageLocators.PASSWORD_INPUT).send_keys(EXISTING_USER_PASSWORD)
+
+        # Нажимаем кнопку "Войти"
+        driver.find_element(*LoginPageLocators.LOGIN_BUTTON).click()
+
+        # Ждём загрузки главной страницы авторизованного пользователя
+        WebDriverWait(driver, 5).until(
+            EC.visibility_of_element_located(MainPageLocators.PLACE_ORDER_BUTTON)
+        )
+
+        # Переходим в личный кабинет
+        driver.find_element(*MainPageLocators.PERSONAL_ACCOUNT_BUTTON).click()
+
+        # Ждём загрузки страницы профиля
+        WebDriverWait(driver, 5).until(
+            EC.visibility_of_element_located(ProfilePageLocators.LOGOUT_BUTTON)
+        )
+
+        # Нажимаем кнопку "Конструктор"
+        driver.find_element(*MainPageLocators.CONSTRUCTOR_BUTTON).click()
+
+        # Ждём загрузки главной страницы
+        WebDriverWait(driver, 5).until(
+            EC.visibility_of_element_located(MainPageLocators.PLACE_ORDER_BUTTON)
+        )
+
+        # Проверяем, что открылась главная страница
+        assert driver.current_url == BASE_URL
