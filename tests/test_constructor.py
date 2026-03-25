1cde15e3b607e8a07d703a2ac2aca025c09de29f
Irina Mikheeva <mikheeva.wrk@gmail.com>
2026-03-25 16:09:46 +0800

    test_constructor.py - переход к разделу Соусы

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -7,8 +7,9 @@ from locators import ConstructorPageLocators
 class TestConstructor:
 
     # Проверка перехода к разделу "Булки"
-
+    
     def test_open_buns_tab(self, driver):
+
         # Сначала переходим в раздел "Соусы"
         driver.find_element(*ConstructorPageLocators.SAUCES_TAB).click()
 
@@ -27,3 +28,18 @@ class TestConstructor:
 
         # Проверяем, что вкладка "Булки" активна
         assert driver.find_element(*ConstructorPageLocators.ACTIVE_BUNS_TAB).is_displayed()
+
+    # Проверка перехода к разделу "Соусы"
+
+    def test_open_sauces_tab(self, driver):
+
+        # Переходим в раздел "Соусы"
+        driver.find_element(*ConstructorPageLocators.SAUCES_TAB).click()
+
+        # Ждём, пока вкладка "Соусы" станет активной
+        WebDriverWait(driver, 5).until(
+            EC.visibility_of_element_located(ConstructorPageLocators.ACTIVE_SAUCES_TAB)
+        )
+
+        # Проверяем, что вкладка "Соусы" активна
+        assert driver.find_element(*ConstructorPageLocators.ACTIVE_SAUCES_TAB).is_displayed()
